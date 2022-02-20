a35ece2f23c4319d03bf3daf5b9aa1947d7b065b
wook <mdir2@naver.com>
2022-02-20 18:04:45 +0900

    kotlin-minesweeper-6 Blocks 컬렉션을 만들고 지뢰를 심는 책임을 해당 객체에게 위임

--- src/main/kotlin/com/minsweeper/block/Coordinate.kt
+++ src/main/kotlin/com/minsweeper/block/Coordinate.kt
@@ -4,6 +4,8 @@ data class Coordinate(
     val x: Int = 0,
     val y: Int = 0
 ) {
+    constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
+
     init {
         require(x >= 0) { "x좌표 음수불가" }
         require(y >= 0) { "y좌표 음수불가" }
