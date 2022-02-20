7f17f958b03186b418c3803fd0b064bbd8909599
wook <mdir2@naver.com>
2022-02-20 18:04:45 +0900

    KM -6 Blocks 컬렉션을 만들고 지뢰를 심는 책임을 해당 객체에게 위임

--- src/test/kotlin/com/minsweeper/block/BoardTest.kt
+++ src/test/kotlin/com/minsweeper/block/BoardTest.kt
@@ -1,5 +1,6 @@
 package com.minsweeper.block
 
+import com.minsweeper.blocks
 import com.minsweeper.board
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -8,42 +9,42 @@ class BoardTest {
 
     @Test
     fun `x by y 형태로 보드를 생성할 수 있다`() {
-        val coordinate = Coordinate(3, 5)
+        val coordinate = Coordinate(3 to 5)
 
-        val board = board(coordinate)
+        val board = board(blocks(coordinate))
 
-        assertThat(board.coordinate).isEqualTo(coordinate)
+        assertThat(board.getBlockCoordinate()).isEqualTo(coordinate)
     }
 
     @Test
     fun `정해진 개수만큼 지뢰를 심는다`() {
         val board = board()
 
-        board.plantMine(Coordinate(0, 2))
-        board.plantMine(Coordinate(1, 1))
-        board.plantMine(Coordinate(2, 0))
+        board.layMine(Coordinate(0 to 2))
+        board.layMine(Coordinate(1 to 1))
+        board.layMine(Coordinate(2 to 0))
 
-        assertThat(board.getBlock(Coordinate(0, 2))).isExactlyInstanceOf(MineBlock::class.java)
-        assertThat(board.getBlock(Coordinate(1, 1))).isExactlyInstanceOf(MineBlock::class.java)
-        assertThat(board.getBlock(Coordinate(2, 0))).isExactlyInstanceOf(MineBlock::class.java)
+        assertThat(board.getBlock(Coordinate(0 to 2))).isExactlyInstanceOf(MineBlock::class.java)
+        assertThat(board.getBlock(Coordinate(1 to 1))).isExactlyInstanceOf(MineBlock::class.java)
+        assertThat(board.getBlock(Coordinate(2 to 0))).isExactlyInstanceOf(MineBlock::class.java)
     }
 
     @Test
     fun `보드에 있는 블록중에 하나를 좌표로 가져올 수 있다`() {
         val board = board()
 
-        board.plantMine(Coordinate(0, 2))
+        board.layMine(Coordinate(0 to 2))
 
-        assertThat(board.getBlock(Coordinate(0, 0))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(0, 1))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(0, 2))).isExactlyInstanceOf(MineBlock::class.java)
+        assertThat(board.getBlock(Coordinate(0 to 0))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(0 to 1))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(0 to 2))).isExactlyInstanceOf(MineBlock::class.java)
 
-        assertThat(board.getBlock(Coordinate(1, 0))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(1, 1))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(1, 2))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(1 to 0))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(1 to 1))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(1 to 2))).isExactlyInstanceOf(BlankBlock::class.java)
 
-        assertThat(board.getBlock(Coordinate(2, 0))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(2, 1))).isExactlyInstanceOf(BlankBlock::class.java)
-        assertThat(board.getBlock(Coordinate(2, 2))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(2 to 0))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(2 to 1))).isExactlyInstanceOf(BlankBlock::class.java)
+        assertThat(board.getBlock(Coordinate(2 to 2))).isExactlyInstanceOf(BlankBlock::class.java)
     }
 }
