d2b6213528e4db6f2fd033ce71781ff89a59f4dc
Woogie Kim <woogie.kim@cyworldlabs.com>
2022-02-27 16:18:20 +0900

    kotlin-minesweeper-8 생성되는 숫자블록은 최대 8개 최소 1개의 숫자를 가진다

--- src/main/kotlin/com/minsweeper/block/NumberBlock.kt
+++ src/main/kotlin/com/minsweeper/block/NumberBlock.kt
@@ -4,6 +4,11 @@ class NumberBlock(
     val number: Int,
     val coordinate: Coordinate,
 ) : SimpleBlock(coordinate, "$number") {
+    init {
+        require(number > MINIMUM) { "숫자는 $MINIMUM 보다 커야 함" }
+        require(number <= MAXIMUM) { "숫자는 $MAXIMUM 보다 작거나 같아야 함" }
+    }
+
     override fun open() {
         check(this.status != BlockStatus.OPEN) { "이미 오픈한 블록" }
 
