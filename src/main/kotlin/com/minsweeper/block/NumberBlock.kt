0de1671a0b3e798bd584b2577dcff1642d5fab28
Woogie Kim <mdir2kor@gmail.com>
2022-02-27 16:12:36 +0900

    KM-8 빠진 유효성검사 추가

--- src/main/kotlin/com/minsweeper/block/NumberBlock.kt
+++ src/main/kotlin/com/minsweeper/block/NumberBlock.kt
@@ -4,9 +4,19 @@ class NumberBlock(
     val number: Int,
     val coordinate: Coordinate,
 ) : SimpleBlock(coordinate, "$number") {
+    init {
+        require(number > MINIMUM) { "숫자는 $MINIMUM 보다 커야 함" }
+        require(number <= MAXIMUM) { "숫자는 $MAXIMUM 보다 작거나 같아야 함" }
+    }
+
     override fun open() {
         check(this.status != BlockStatus.OPEN) { "이미 오픈한 블록" }
 
         this.status = BlockStatus.OPEN
+    }
+
+    companion object {
+        const val MINIMUM = 0
+        const val MAXIMUM = 8
     }
 }
